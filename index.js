2ea97b47908dd2f763509b17839845804d0d6601
tanawin.sg <tanawin.sg@gmail.com>
2022-06-14 22:55:49 +0700

    router

--- index.js
+++ index.js
@@ -1,20 +1,8 @@
 const express = require('express')
-const path = require('path')
+const router = require('./routes/myRouter')
+
 const app = express()
-
-const indexPage = path.join(__dirname,'/templates/index.html')
-
-//app.use("/home",(req,res)=>{  // เหมาะสำหรับ 1-2 path ถ้า path เยอะๆจะใช้ method get
-app.get("/home",(req,res)=>{
-    res.status(200)
-    res.type("text/html")
-    res.sendFile(indexPage)
-})
-
-app.get("/product",(req,res)=>{
-    res.sendFile(path.join(__dirname,'/templates/product1.html'))
-    // res.send("Hello Product")
-})
+app.use(router)
 
 app.listen(8080,()=>{
     console.log("Start server port 8080")
