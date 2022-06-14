d40a8c860277631531317f0a9987bae56762eb72
tanawin.sg <tanawin.sg@gmail.com>
2022-06-14 23:15:08 +0700

    router parameter

--- routes/myRouter.js
+++ routes/myRouter.js
@@ -12,8 +12,21 @@ router.get("/home",(req,res)=>{
     res.sendFile(indexPage)
 })
 
-router.get("/product",(req,res)=>{
-    res.sendFile(path.join(__dirname,'../templates/product1.html'))  //เปิดไฟล์อ่านข้อมูลและส่งข้อมูลกลับ
+router.get("/product/:id",(req,res)=>{
+    const productID = req.params.id
+
+    if(productID == 1){
+        res.sendFile(path.join(__dirname,'../templates/product1.html'))  //เปิดไฟล์อ่านข้อมูลและส่งข้อมูลกลับ
+    }else if(productID == 2){
+        res.sendFile(path.join(__dirname,'../templates/product2.html'))
+    }else if (productID == 3){
+        res.sendFile(path.join(__dirname,'../templates/product3.html'))
+    }else{
+        res.status(404)
+        res.send("<h1>404 Page Not Found</h1>")
+    }
+    
+
 })
 /*app.get("/product",(req,res)=>{
     res.sendFile(path.join(__dirname,'/templates/product1.html'))
