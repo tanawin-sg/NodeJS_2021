6b865c3c79eceb51a4032ab0588f9d4dc99352ba
tanawin.sg <tanawin.sg@gmail.com>
2022-06-28 17:16:08 +0700

    ejs array

--- routes/myRouter.js
+++ routes/myRouter.js
@@ -4,11 +4,11 @@ const router = express.Router() //เรียกใช้ class router
 const path = require('path') //เรียกใช้ module path
 
 router.get('/',(req,res)=>{
-    const name = "Tanawin"
-    const age = 35
-    const address = "<h3>กรุงเทพ</h3>"
+    const products = ["เสื้อ","พัดลม","หูฟัง","คีย์บอร์ด"]
+    const sname = "Tanawin"
+    const lname = "<h2>Lastname : Sangjan</h2>"
     //send prm to file ejs
-    res.render('index',{name:name,age:age,address:address}) //index.ejs
+    res.render('index',{products:products,sname:sname,lname:lname}) //index.ejs
 })
 
 module.exports = router
