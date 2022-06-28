f3b23dc98fa642d26d9d32a7ac65b8724de7815e
tanawin.sg <tanawin.sg@gmail.com>
2022-06-28 16:52:28 +0700

    ejs condition if and for

--- routes/myRouter.js
+++ routes/myRouter.js
@@ -4,7 +4,11 @@ const router = express.Router() //เรียกใช้ class router
 const path = require('path') //เรียกใช้ module path
 
 router.get('/',(req,res)=>{
-    res.render('index.ejs')
+    const name = "Tanawin"
+    const age = 35
+    const address = "<h3>กรุงเทพ</h3>"
+    //send prm to file ejs
+    res.render('index',{name:name,age:age,address:address}) //index.ejs
 })
 
 module.exports = router
